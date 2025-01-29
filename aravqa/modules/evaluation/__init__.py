26a23c54c685631204fe60570a5fa0ed77d5aec9
shahadMAlshalawi <researchshahad@gmail.com>
2025-01-30 01:03:45 +0300

    adding Evalution  Classes

--- aravqa/modules/evaluation/__init__.py
+++ aravqa/modules/evaluation/__init__.py
@@ -1,3 +1,7 @@
 from .base import BaseEvaluator
 from .bleu_evaluator import BLEUEvaluator
 from .accuracy_evaluator import AccuracyEvaluator
+from .bert_score_evaluator import BERTScoreEvaluator
+from .rouge_evaluator import ROUGEEvaluator
+from .meteor_evaluator import METEOREvaluator
+from .google_bleu_evaluator import GoogleBLEUEvaluator
